99f894d106afc0e65117d76f5c50f0fb545a3379
Kateryna Kalinchenko <kate.kalinchenko@gmail.com>
2023-08-04 21:09:21 +0300

    init commit

---
+++ test_swift/test_swift/test_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_swiftApp.swift
+//  test_swift
+//
+//  Created by Kate on 04.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct test_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
